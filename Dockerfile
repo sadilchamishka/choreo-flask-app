b882edfd8e09c5b58aa4c67de749aa066c556bd9
sadilchamishka <sandilchamishka@gmail.com>
2024-11-17 22:43:28 +0530

    Add required files

--- Dockerfile
+++ Dockerfile
@@ -1,31 +1,38 @@
-# Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com/) All Rights Reserved.
-
-# WSO2 LLC. licenses this file to you under the Apache License,
-# Version 2.0 (the "License"); you may not use this file except
-# in compliance with the License.
-# You may obtain a copy of the License at
-
-# http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied. See the License for the
-# specific language governing permissions and limitations
-# under the License.
-
-FROM python:alpine
+# Development stage
+FROM python:alpine AS development
 
 WORKDIR /python-docker
 
+# Copy the requirements file and install dependencies
+COPY requirements.txt requirements.txt
+RUN pip3 install -r requirements.txt
+
+# Copy the application code
+COPY . .
+RUN ls
+
+# Run tests
+CMD ["python3", "test_app.py", "-v"]
+
+# Production stage
+FROM python:alpine AS production
+
+WORKDIR /python-docker
+
+# Copy only the necessary files for production
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 
 COPY . .
-RUN ls
+
 # Create a new user with UID 10016
 RUN addgroup -g 10016 choreo && \
     adduser  --disabled-password  --no-create-home --uid 10016 --ingroup choreo choreouser
+
+# Switch to the new user
 USER 10016
+
 EXPOSE 5000
-CMD [ "flask", "run", "--host=0.0.0.0"]
+
+# Start the Flask application
+CMD ["flask", "run", "--host=0.0.0.0"]
--- app.py
+++ app.py
@@ -1,90 +1,55 @@
-# Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com/) All Rights Reserved.
+from flask import Flask, jsonify, request
 
-# WSO2 LLC. licenses this file to you under the Apache License,
-# Version 2.0 (the "License"); you may not use this file except
-# in compliance with the License.
-# You may obtain a copy of the License at
-
-# http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied. See the License for the
-# specific language governing permissions and limitations
-# under the License.
-
-import json
-import ast
-from flask import Flask, request
-from types import SimpleNamespace
 app = Flask(__name__)
 
-# defines initial reservations
-with open('data.txt') as f:
-    reservations = ast.literal_eval(f.read())
+books = []
 
-# route relevant to the reservation management
-@app.route('/rs/reservation/<reservationId>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def reservation_management(reservationId):
-    if request.method == "GET":
-        return str("Your reservation details: " + getReservation(reservationId))
-    elif request.method == "POST":
-        return str("Your added reservation details: " + addReservation(request))
-    elif request.method == "PUT":
-        return "Reservation updated: " + updateReservation(reservationId, request) ;
-    elif request.method == "DELETE":
-        return "Reservation deleted: " + deleteReservation(reservationId)
+# helper function to generate IDs
+def generate_id():
+    return len(books) + 1
 
-# route relevant to the hello world
-@app.route('/rs/healthCheck', methods=['GET'])
-def health_check():
-    return "Hello, Welcome to the simple reservation management app!"
+# endpoint to get all books
+@app.route('/reading-list/books', methods=['GET'])
+def get_books():
+    return jsonify({'books': books})
 
-# route relevant to get all reservations
-@app.route('/rs/reservations', methods=['GET'])
-def get_reservations():
-    return str(json.dumps(reservations))
+# endpoint to add a new book
+@app.route('/reading-list/books', methods=['POST'])
+def add_book():
+    data = request.get_json()
+    book = {
+        'id': generate_id(),
+        'author': data['author'],
+        'name': data['name'],
+        'status': data['status']
+    }
+    books.append(book)
+    return jsonify(book)
 
-# gives a reservation created by the user considering the reservationId
-def getReservation(reservationId):
-    print("getReservation")
-    for reservation in reservations:
-        if reservation["reservationId"] == reservationId:
-            print(type(json.dumps(reservation)))
-            return str(json.dumps(reservation))
-    return None
+# endpoint to get a book by ID
+@app.route('/reading-list/books/<int:book_id>', methods=['GET'])
+def get_book(book_id):
+    for book in books:
+        if book['id'] == book_id:
+            return jsonify(book)
+    return '', 404
 
-# adds a reservation to the list of reservations
-def addReservation(request):
-    x = json.loads(request.get_data(), object_hook=lambda d: SimpleNamespace(**d))
-    reservations.append({
-        "reservationCreator" : x.reservationCreator,
-		"reservationId" : x.reservationId,
-		"contact" : x.contact,})
-    print(reservations)
-    return str(request.get_data())
+# endpoint to delete a book by ID
+@app.route('/reading-list/books/<int:book_id>', methods=['DELETE'])
+def delete_book(book_id):
+    global books
+    books = [book for book in books if book['id'] != book_id]
+    return '', 204
 
-# deletes a reservation from the list of reservations considering the reservationId
-def deleteReservation(reservationId):
-    for reservation in reservations:
-        if reservation["reservationId"] == reservationId:
-            reservations.remove(reservation)
-            return str(json.dumps(reservation))
-    return reservationId
+# endpoint to update a book status by ID
+@app.route('/reading-list/books/<int:book_id>', methods=['PUT'])
+def update_book_status(book_id):
+    data = request.get_json()
+    for book in books:
+        if book['id'] == book_id:
+            book['status'] = data['status']
+            return jsonify(book)
+    return '', 404
 
-# updates a reservation from the list of reservations considering the reservationId
-def updateReservation(reservationId, request):
-    for reservation in reservations:
-        if reservation["reservationId"] == reservationId:
-            reservations.remove(reservation)
-            x = json.loads(request.get_data(), object_hook=lambda d: SimpleNamespace(**d))
-            reservations.append({
-                "reservationCreator" : x.reservationCreator,
-		        "reservationId" : x.reservationId,
-		        "contact" : x.contact,})
-    print(reservations)
-    return reservationId
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
