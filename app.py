df60562abae12f95f7237c60b06c7cf04b39d596
sadilchamishka <sandilchamishka@gmail.com>
2024-12-17 13:20:45 +0530

    Revert "test"
    This reverts commit e0676d482ee082896f5d7b9f5d2b7f34a09d4598.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import pickle
+#import pandas as pd
 
 app = Flask(__name__)
 
@@ -27,6 +28,7 @@ def predict():
                 return f"Error: Query parameter '{param}' must be a valid number"
 
         # Reshaping the list to match the expected input format
+        #df_new = pd.DataFrame([data_list])
 
         # Load the saved model
         loaded_model = pickle.load(open('naive_bayes_model.sav', 'rb'))
