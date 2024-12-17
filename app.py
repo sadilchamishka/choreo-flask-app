1a3541ec4f0bf7f28079226e338f4c7d31042ec3
sadilchamishka <sandilchamishka@gmail.com>
2024-12-17 16:09:35 +0530

    test

--- app.py
+++ app.py
@@ -4,14 +4,8 @@ import pandas as pd
 
 app = Flask(__name__)
 
-books = []
-
-# helper function to generate IDs
-def generate_id():
-    return len(books) + 1
-
 # endpoint to get all books
-@app.route('/reading-list/books', methods=['GET'])
+@app.route('/reading-list/predict', methods=['GET'])
 def get_books():
     try:
         # List of expected parameters
