61736a0e9043fc8c61ce3a82f23a8b18d2c5fa91
sadilchamishka <sandilchamishka@gmail.com>
2024-12-17 13:20:45 +0530

    Revert "test"
    This reverts commit 12c59638d35ccbcdf57880c50188959e36914702.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 import pickle
-#import pandas as pd
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -28,14 +28,13 @@ def predict():
                 return f"Error: Query parameter '{param}' must be a valid number"
 
         # Reshaping the list to match the expected input format
-        #df_new = pd.DataFrame([data_list])
+        df_new = pd.DataFrame([data_list])
 
         # Load the saved model
         loaded_model = pickle.load(open('naive_bayes_model.sav', 'rb'))
 
         # Predict the probability
-        #return str(loaded_model.predict_proba(df_new)[0][0])
-        return "Success"
+        return str(loaded_model.predict_proba(df_new)[0][0])
 
 
 
